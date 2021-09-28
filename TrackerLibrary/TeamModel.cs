41d0b24bc15092369b6779f7f4d259a1b200f1ce
Dimitar Vashkov <dimitarvashkov@outlook.com>
2021-09-28 03:02:13 +0300

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        // You can do this in newer C# (since C#6) otherwise use constructor.
+        public string TeamName { get; set; }
+    }
+}
---
+++ TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {   /// <summary>
+        /// Represents the name of the tournament
+        /// </summary>
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+
+        /// <summary>
+        /// Represents all the teams in the tournament as a list
+        /// </summary>
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+
+        /// <summary>
+        /// Represents all the prizes as a list
+        /// </summary>
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        /// <summary>
+        /// Represents all the matchups as a list from a list of matchups
+        /// </summary>
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+    }
+}
